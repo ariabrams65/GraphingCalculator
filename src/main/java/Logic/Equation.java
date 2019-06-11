7e2e73ab4adbc70c22deb630958751c6f1382d4f
Ari Abrams <ariabrams65@gmail.com>
2019-06-10 20:21:15 -0700

    added post-fix alg

--- src/main/java/Logic/Equation.java
+++ src/main/java/Logic/Equation.java
@@ -1,8 +1,6 @@
 package Logic;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import java.util.Stack;
 
 public class Equation {
 
@@ -14,17 +12,52 @@ public class Equation {
 
     public double getYValue(double x) {
         String newEquation = equation.replace("x", Double.toString(x));
-        
-         String value = "-1";
-        try {
-            
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
-        value = engine.eval(newEquation).toString();
-        
-        } catch (ScriptException e) {
-            System.out.println(e.getMessage());
+
+        Stack<Double> stack = new Stack<Double>();
+
+        String[] list = newEquation.split(" ");
+
+        for (String t : list) {
+
+            if (isNumber(t)) {
+                stack.push(Double.parseDouble(t));
+
+            } else if (isOperator(t)) {
+                double op2 = stack.pop();
+                double op1 = stack.pop();
+
+                stack.push(evaluate(op1, op2, t));
+            }
         }
-        return Double.parseDouble(value);
-        
+        return stack.pop();
+    }
+
+    private static boolean isNumber(String s) {
+        try {
+            Double.parseDouble(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isOperator(String t) {
+        return t.matches("[+-/*^]"); 
+    }
+
+    private static double evaluate(double op1, double op2, String t) {
+        if (t.equals("+")) {
+            return op1 + op2;
+
+        } else if (t.equals("-")) {
+            return op1 - op2;
+
+        } else if (t.equals("*")) {
+            return op1 * op2;
+            
+        } else if (t.equals("^")) {
+            return Math.pow(op1, op2);
+        }
+        return -1;
     }
 }
